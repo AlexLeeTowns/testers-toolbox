93977806cea1a33802f882f3ae7c6abda0409b81
AlexLeeTowns <alexlee@towns.dk>
2022-02-26 23:52:00 -0400

    extract switch to function

--- cmd/lorem.go
+++ cmd/lorem.go
@@ -16,16 +16,39 @@ import (
 	"github.com/spf13/pflag"
 )
 
+func readLorem(name string, count int) string {
+	switch name {
+	case "char":
+		fml, err := l.ReadLoremByCharacterCount(os.DirFS("./pkg/loremipsum"), "lorem.txt", count)
+		if err != nil {
+			fmt.Print(err)
+			os.Exit(1)
+		}
+		return fml
+	case "paragraph":
+		fml, err := l.ReadLoremByParagraph(os.DirFS("./pkg/loremipsum"), "lorem.txt", count)
+		if err != nil {
+			fmt.Print(err)
+			os.Exit(1)
+		}
+		return fml
+	case "word":
+		fml, err := l.ReadLoremByWordCount(os.DirFS("./pkg/loremipsum"), "lorem.txt", count)
+		if err != nil {
+			fmt.Print(err)
+			os.Exit(1)
+		}
+		return fml
+	}
+
+	return ""
+}
+
 // loremCmd represents the lorem command
 var loremCmd = &cobra.Command{
 	Use:   "lorem",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Copy lorem ipsum text to clipboard",
+	Long:  `Copy lorem ipsum text to clipboard by word count, character count, or paragraph count.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		var result string
 
@@ -35,29 +58,7 @@ to quickly create a Cobra application.`,
 				fmt.Print(err)
 				os.Exit(1)
 			}
-			switch f.Name {
-			case "char":
-				fml, err := l.ReadLoremByCharacterCount(os.DirFS("./pkg/loremipsum"), "lorem.txt", c)
-				if err != nil {
-					fmt.Print(err)
-					os.Exit(1)
-				}
-				result += fml
-			case "paragraph":
-				fml, err := l.ReadLoremByParagraph(os.DirFS("./pkg/loremipsum"), "lorem.txt", c)
-				if err != nil {
-					fmt.Print(err)
-					os.Exit(1)
-				}
-				result += fml
-			case "word":
-				fml, err := l.ReadLoremByWordCount(os.DirFS("./pkg/loremipsum"), "lorem.txt", c)
-				if err != nil {
-					fmt.Print(err)
-					os.Exit(1)
-				}
-				result += fml
-			}
+			result += readLorem(f.Name, c)
 		})
 
 		clipboard.WriteAll(result)
@@ -67,17 +68,6 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(loremCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// loremCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// loremCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	loremCmd.Flags().IntP("word", "w", 0, "Retrieve lorem ipsum by word count. If --count is not set, will return 4 words.")
 	loremCmd.Flags().IntP("char", "c", 0, "Retrieve lorem ipsum by character count. If --count is not set, will return 5 letters")
 	loremCmd.Flags().IntP("paragraph", "p", 0, "Retrieve lorem ipsum by paragraph. If --count is not set, will return 1 paragraph")
 }
